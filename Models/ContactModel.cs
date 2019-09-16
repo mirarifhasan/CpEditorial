69b5482818185f863b357aa3f653c974074eded4
mirarifhsan <arif.ishan05@gmail.com>
2019-09-17 00:37:12 +0600

    register css minor update

---
+++ Models/ContactModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CpEditorial.Models
+{
+    public class ContactModel
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ Models/DBHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CpEditorial.Models
+{
+    public class DBHelper
+    {
+        public string connectionString = @"Data Source = DESKTOP-02U52QL\SQLEXPRESS; Initial Catalog = cpEditorial; Integrated Security=True";
+        
+        public string getConnectionString()
+        {
+            return connectionString;
+        }
+    }
+}
