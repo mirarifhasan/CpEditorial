664f40cf3cf814e0139b91f331c5a3c43c6a0ca7
mirarifhsan <arif.ishan05@gmail.com>
2019-09-22 01:14:45 +0600

    login backend done

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -1,6 +1,7 @@
 ﻿using CpEditorial.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -17,33 +18,101 @@ namespace CpEditorial.Controllers
         {
             return View();
         }
+
         public ActionResult LogIn()
         {
-            return View();
-        }
-
-        public ActionResult SignUp()
-        {
-            if (TempData["message"] != null)
+            if (TempData["message"] != null) //It will true when user not login successfully
                 ViewBag.Error = TempData["message"].ToString();
             return View();
         }
 
-        // It execute/call from AddUser method, when Password and ConfirmPassword will not match
-        public ActionResult Verify()
+        [HttpPost]
+        public ActionResult VerifyUser(UserModel userModel)
         {
-            TempData["message"] = "Password not match";
-            return RedirectToAction("SignUp", "Register");
+            // Check email registered or not
+            var dtblUser = new DataTable();
+            using (var sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                string query = "select * from [User] where Email = @email";
+                var sqlDa = new SqlDataAdapter(query, sqlCon);
+                sqlDa.SelectCommand.Parameters.AddWithValue("@email", userModel.email);
+                sqlDa.Fill(dtblUser);
+            }
+            //Ture - Found email in database
+            if (dtblUser.Rows.Count == 1)
+            {
+                dtblUser.Clear();
+                using (var sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "select * from [User] where Email = @email and Password = @password";
+                    var sqlDa = new SqlDataAdapter(query, sqlCon);
+                    sqlDa.SelectCommand.Parameters.AddWithValue("@email", userModel.email);
+                    sqlDa.SelectCommand.Parameters.AddWithValue("@password", userModel.password);
+                    sqlDa.Fill(dtblUser);
+                }
+                // If email and password both match
+                if (dtblUser.Rows.Count == 1)
+                {
+                    userModel.userID = dtblUser.Rows[0][0].ToString();
+                    userModel.name = dtblUser.Rows[0][1].ToString();
+                    userModel.point = Convert.ToInt32(dtblUser.Rows[0][4].ToString());
+                    userModel.type = dtblUser.Rows[0][5].ToString();
+
+                    return RedirectToAction("Index", "Home");
+                }
+                // Email match nut Password not match
+                else
+                {
+                    TempData["message"] = "Password not match";
+                    return RedirectToAction("LogIn", "Register");
+                }
+            }
+            // Email not found in DB
+            else
+            {
+                TempData["message"] = "Email not registered";
+                return RedirectToAction("LogIn", "Register");
+            }
+        }
+
+        public ActionResult SignUp()
+        {
+            if (TempData["message"] != null) //It will true when Password and ConfirmPassword not match
+                ViewBag.Error = TempData["message"].ToString();
+            return View();
         }
 
         [HttpPost]
         public ActionResult AddUser(SignupModel signupModel)
         {
+            // Check Password and ConfirmPassword are match or not
+            if (signupModel.password != signupModel.confirmPassword)
+            {
+                TempData["message"] = "Password not match";
+                return RedirectToAction("SignUp", "Register");
+            }
+
+            // Check the email already exist or not
+            var dtblUser = new DataTable();
             using (var sqlCon = new SqlConnection(connectionString))
             {
-                if (signupModel.password != signupModel.confirmPassword)
-                    return RedirectToAction("Verify", "Register");
+                sqlCon.Open();
+                string query = "select * from [User] where Email = @email";
+                var sqlDa = new SqlDataAdapter(query, sqlCon);
+                sqlDa.SelectCommand.Parameters.AddWithValue("@email", signupModel.email);
+                sqlDa.Fill(dtblUser);
+            }
+            if(dtblUser.Rows.Count == 1)
+            {
+                TempData["message"] = "Email already registered";
+                return RedirectToAction("SignUp", "Register");
+            }
 
+            // Insert the User
+            using (var sqlCon = new SqlConnection(connectionString))
+            {
                 sqlCon.Open();
                 string query = "INSERT INTO [User] VALUES (@Name, @Email, @Password, '0', 'User')";
                 var sqlCmd = new SqlCommand(query, sqlCon);
@@ -52,7 +121,7 @@ namespace CpEditorial.Controllers
                 sqlCmd.Parameters.AddWithValue("@Password", signupModel.password);
                 sqlCmd.ExecuteNonQuery();
             }
-            return RedirectToAction("Index", "Home");            
+            return RedirectToAction("Index", "Home");
         }
         
     }
---
+++ Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CpEditorial.Models
+{
+    public class UserModel
+    {
+        public string userID { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int point { get; set; }
+        public string type { get; set; }
+    }
+}
