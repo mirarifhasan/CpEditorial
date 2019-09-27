2127a114b7a08bcf2f100ee53a55f7cae60eb503
mirarifhsan <arif.ishan05@gmail.com>
2019-09-28 01:08:57 +0600

    dropdown showing done

--- Models/PostFormModel.cs
+++ Models/PostFormModel.cs
@@ -8,13 +8,8 @@ namespace CpEditorial.Models
 {
     public class PostFormModel
     {
-        public List<List<OJTagModel>> ojTagList = new List<List<OJTagModel>>();
         public List<OJTagModel> tagList = new List<OJTagModel>();
         public List<OJTagModel> ojList = new List<OJTagModel>();
-
-        public List<List<OJTagModel>> ojTagListMethod { get; set; }
-        public List<OJTagModel> tagListMethod { get; set; }
-        public List<OJTagModel> ojListMethod { get; set; }
         
         // Constractor
         public PostFormModel()
@@ -31,6 +26,7 @@ namespace CpEditorial.Models
             // Adding all tags in list
             for(int i=0; i<tagTable.Rows.Count; i++)
             {
+                ojTagModel = new OJTagModel();
                 ojTagModel.id = Convert.ToInt32(tagTable.Rows[i][0].ToString());
                 ojTagModel.value = tagTable.Rows[i][1].ToString();
 
@@ -39,25 +35,13 @@ namespace CpEditorial.Models
             // Adding all OJ names in list
             for (int i = 0; i < ojTable.Rows.Count; i++)
             {
+                ojTagModel = new OJTagModel();
                 ojTagModel.id = Convert.ToInt32(ojTable.Rows[i][0].ToString());
                 ojTagModel.value = ojTable.Rows[i][1].ToString();
 
                 ojList.Add(ojTagModel);
             }
-
-            for (int i = 0; i < 2; i++)
-                ojTagList.Add(new List<OJTagModel>());
-
-            ojTagList.Add(tagList);
-            ojTagList.Add(ojList);
         }
-
-        // To call this method, make sure constractor execute successfully
-        public List<List<OJTagModel>> getOJTagList()
-        {
-            return ojTagList;
-        }
-
 
     }
 }
